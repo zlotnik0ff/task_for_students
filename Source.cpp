996a596c6fc07777aac182cc24d41fd0551b2a45
zl0tn1k0ff <flejkee@gmail.com>
2018-02-26 18:51:03 +0300

    Some kind of solution :>

--- Source.cpp
+++ Source.cpp
@@ -1,122 +1,110 @@
-
 #include <iostream>
 
+using namespace std;
+
+template <typename T>
+void printWithTab(T toPrint)
+{
+	cout << toPrint << '\t';
+}
 
 
-struct Pupil {
+struct Pupil
+{
 	int age;
-	int grade;
-	char name[20];
-	char *surname;
-	float avg_mark;
+	char name[50];
+	char surname[60];
+	char grade[60];
+	float avgMark;
+
+	void fillTheFields()
+	{
+		cout << endl;
+		cout << "Age - ";
+		cin >> age;
+		cout << "Name is ";
+		cin >> name;
+		cout << "Surname is ";
+		cin >> surname;
+		cout << "Grade: ";
+		cin >> grade;
+		cout << "Average mark = ";
+		cin >> avgMark;
+		cout << endl;
+	};
+
+	Pupil *next; 
 };
 
+class List
+{
+private:
+	Pupil *head;
 
+public:
 
-
-void InitMas(int *mas, int n) {
-	for (int i = 0; i < n; ++i) {
-		mas[i] = rand() % 101 - 50;
-	}
-}
-
-void PrintMas(int *mas, int n) {
-	for (int i = 0; i < n; ++i) {
-		std::cout << mas[i]<< '\t';
-	}
-	std::cout << std::endl;
-}
-
-void DeleteNegatives(int *&mas, int &n) {
-	int nNegative = 0;
-	for (int i = 0; i < n; ++i) {
-		if (mas[i] < 0) {
-			++nNegative;
+	List() { head = NULL; };
+	~List()
+	{
+		while (head != NULL)
+		{
+			Pupil *temp = head->next;
+			delete head;
+			head = temp;
 		}
-	}
-	n -= nNegative;
-	int *aNewMas = new int[n];
-	int step = 0;
-	for (int i = 0; i < n + nNegative; ++i) {
-		if (mas[i] < 0) {
-			++step;
-			continue;
+	};
+
+	void add(Pupil &pupil)
+	{
+		Pupil *temp = new Pupil;
+		temp->next = head;
+
+		temp->age = pupil.age;
+		strcpy(temp->name, pupil.name);
+		strcpy(temp->surname, pupil.surname);
+		strcpy(temp->grade, pupil.grade);
+		temp->avgMark = pupil.avgMark;
+
+		head = temp;
+	};
+	
+
+	void printAllFields()
+	{
+		Pupil *temp = head;
+
+		while (temp != NULL)
+		{
+			printWithTab(temp->age);
+			printWithTab(temp->name);
+			printWithTab(temp->surname);
+			printWithTab(temp->grade);
+			printWithTab(temp->avgMark);
+			cout << endl;
+			
+			temp = temp->next;
 		}
-		aNewMas[i - step] = mas[i];
-	}
-	delete[]mas;
-	mas = aNewMas;
-	PrintMas(mas, n);
-}
-
-
-
-void DeleteByCondition(int *&mas, int &n, bool(*pred)(int)) {
-	int nNegative = 0;
-	for (int i = 0; i < n; ++i) {
-		if (pred(mas[i])) {
-			++nNegative;
-		}
-	}
-	n -= nNegative;
-	int *aNewMas = new int[n];
-	int step = 0;
-	for (int i = 0; i < n + nNegative; ++i) {
-		if (pred(mas[i])) {
-			++step;
-			continue;
-		}
-		aNewMas[i - step] = mas[i];
-	}
-	delete[]mas;
-	mas = aNewMas;
-	PrintMas(mas, n);
-}
-
-
-bool IsPositive(int item) {
-	return item > 0;
-}
-
-bool IsNegative(int item) {
-	return item < 0;
-}
-
-bool IsOdd(int item) {
-	return item % 2;
-}
-
-void PrintPupilInfo(Pupil pup) {
-	std::cout << "Name:=" << pup.name << "Age:" << pup.age << std::endl;
-}
+		cout << endl;
+	};
+};
 
 
 int main()
 {
-	Pupil me;
-	me.age = 18;
-	me.grade = 99;
-	strcpy(me.name, "Karl");
-	me.avg_mark = 99.5f;
-	PrintPupilInfo(me);
+	Pupil pupil;
+	int quantity;  
+	List lst;
 
-	void(*some_f_pointer)(int *, int) = InitMas;
-	const int* a;
-	int const* b;
-	int* const p=NULL;
+	cout << "Quantity = ";
+	cin >> quantity;
 
-	int n;
-	std::cin >> n;
-	int *mas = new int[n];
+	for (int i = 0; i < quantity; i++)
+	{
+		pupil.fillTheFields();
+		lst.add(pupil);
+	}
 
-	some_f_pointer(mas, n);
-	PrintMas(mas, n);
-
-	DeleteByCondition(mas, n, [](int a) -> bool { return a % 2; });
-	
-	PrintMas(mas, n);
-	delete[] mas;
-
-	system("pause");
-	return 0;
+	cout << endl;
+	lst.printAllFields();
+	getchar();
 }
